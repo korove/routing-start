9469be5628a812fdbf4a901da2e5c8b06a9d5a39
nattaporn.kig <undfined1@gmail.com>
2017-11-04 17:08:30 +0700

    135. Protecting Child (Nested) Routes with canActivateChild

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,11 @@ const appRoutes: Routes = [
   { path: 'users', component: UsersComponent, children: [
     { path: ':id/:name', component: UserComponent }
   ] },
-  { path: 'servers', canActivate: [AuthGuard], component: ServersComponent, children: [
+  { path: 'servers',
+    // canActivate: [AuthGuard],
+    component: ServersComponent,
+    canActivateChild: [AuthGuard],
+    children: [
     { path: ':id', component: ServerComponent },
     { path: ':id/edit', component: EditServerComponent }
   ] },

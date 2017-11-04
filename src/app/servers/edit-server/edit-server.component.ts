323a0cdfc772eb1047827d866db5e5314850d9ec
nattaporn.kig <undfined1@gmail.com>
2017-11-04 20:38:04 +0700

    137. Controlling Navigation with canDeactivate

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -2,13 +2,15 @@ import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
 import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { CanComponentDeactivate } from './can-deactivate-guard.service';
 
 @Component({
   selector: 'app-edit-server',
   templateUrl: './edit-server.component.html',
   styleUrls: ['./edit-server.component.css']
 })
-export class EditServerComponent implements OnInit {
+export class EditServerComponent implements OnInit, CanComponentDeactivate {
   server: {id: number, name: string, status: string};
   serverName = '';
   serverStatus = '';
@@ -29,7 +31,8 @@ export class EditServerComponent implements OnInit {
         }
       );
     this.route.fragment.subscribe();
-    this.server = this.serversService.getServer(1);
+    const id = +this.route.snapshot.params['id'];
+    this.server = this.serversService.getServer(id);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
   }
@@ -38,6 +41,18 @@ export class EditServerComponent implements OnInit {
     this.serversService.updateServer(this.server.id, {name: this.serverName, status: this.serverStatus});
     this.changesSaved = true;
     this.router.navigate(['../'], {relativeTo: this.route});
+  }
+
+  canDeactivate(): boolean | Observable<boolean> | Promise<boolean>{
+    if(!this.allowEdit){
+      return true;
+    }
+    if((this.serverName !== this.server.name || this.serverStatus !== this.server.status)
+      && !this.changesSaved){
+      return confirm('Do you want to discard the changes?');
+    }else{
+      return true;
+    }
   }
 
 }
